3d4441e45d24c1d1639fc2e62eb8d9d4cdae4acb
ASSASSIN-363 <mssrk2001@gmail.com>
2020-07-08 07:39:34 +0530

    Task - 7

---
+++ Context_Menu/app/src/main/java/com/example/context_menu/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.context_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ListView list;
+    List<String> stringList = Arrays.asList("Hello", "World!", "How", "Are", "You");
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.list);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, stringList);
+        list.setAdapter(adapter);
+        registerForContextMenu(list);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.context_menu,menu);
+    }
+}
